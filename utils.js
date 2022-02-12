e244abc5b9aa25a52b3174c0794dd5a172284cef
Vidhu Shekhar <twrvidhu18@gmail.com>
2022-02-12 12:43:50 +0530

    forms

--- utils.js
+++ utils.js
@@ -5,7 +5,9 @@ const created_btn = document.querySelector('#created-btn');
 
 createbtn.addEventListener('click', () => {
     prcontainer.style.display = "unset"
+
 })
 created_btn.addEventListener('click', () => {
     prcontainer.style.display = "none"
+
 })
